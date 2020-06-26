ccb6d5807e1eb68d90bf64e4bd2c2fdc60efd62c
DiegoOTdC <studioartdiego@gmail.com>
2020-06-26 20:07:17 +0200

    initialize server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+
+const page = `<html>
+    <title>Diégo Teixeira da Costa</title>
+    <h1>Why can't a bike ride on its own?</h1>
+    <p>Because it's two tired!</p>
+    <h2><marquee>!!!!hahahaha!!!!</marquee><h2>
+</html>`
+
+app.get(
+    '/',
+    (request, response) => response.send(page)
+)
+
+const port = 3000
+
+app.listen(
+    port,
+    console.log(`Listening on :${port}`)
+)
